ddfb4be27c473f20f75e38894cc092d81fcf023a
Maksimdon1 <111438708+Maksimdon1@users.noreply.github.com>
2023-08-14 14:34:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+app.get("/server/temp", function(req, res) {
+    res.send('123')
+
+});
+app.get("/", function(req, res) {
+    res.send('12333333')
+
+});
+
 app.listen(process.env.PORT || 3000)
